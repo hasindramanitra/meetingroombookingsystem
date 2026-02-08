488af90205ef7718679aaa8faa37d1073eb4a26f
Briano <briano.manitra@gmail.com>
2026-02-08 09:33:28 +0200

    project meetingroombookingsystem initialized

---
+++ src/main/java/com/briano/meeting_room_booking_system/MeetingRoomBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.briano.meeting_room_booking_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingRoomBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingRoomBookingSystemApplication.class, args);
+	}
+
+}
